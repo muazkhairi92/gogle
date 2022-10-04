354f7b240911ff831bc276ac6b173a6876e536f2
muazkhairi92 <muazkhairi@alumni.iastate.edu>
2022-10-04 14:29:57 +0800

    edit gogle

--- src/main.jsx
+++ src/main.jsx
@@ -6,15 +6,21 @@ import './index.css'
 import AuthProvider from './providers/AuthProvider'
 import {Provider as ReduxProvider} from 'react-redux'
 import store from './store'
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const queryClient = new QueryClient;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
     <ReduxProvider store={store}>
     <AuthProvider>
     <App />
     </AuthProvider>
     </ReduxProvider>
+    </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
 )
--- src/view/UserDetail.jsx
+++ src/view/UserDetail.jsx
@@ -2,34 +2,45 @@
 import {useSelector, useDispatch} from 'react-redux';
 import { useEffect,useState } from "react";
 import { getUser } from '../api/users';
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
 
 
 const UserDetail = () => {
 
 
-  const {users} = useSelector(state=>state.user);
-  const dispatch = useDispatch();  
-  const setUsers = (data)=>dispatch({type:'SET_USER_STATE',payload:{users:data}})
-  const fetchUsers = async()=>{
-    const response = await getUser();
-    console.log(response.data);
-    if(response.status === 200 && response.data){
-        setUsers(response.data);
-    }
+  // const {users} = useSelector(state=>state.user);
+  // const dispatch = useDispatch();  
+  // const setUsers = (data)=>dispatch({type:'SET_USER_STATE',payload:{users:data}})
+  // const fetchUsers = async()=>{
+  //   const response = await getUser();
+  //   console.log(response.data);
+  //   if(response.status === 200 && response.data){
+  //       setUsers(response.data);
+  //   }
     
-};
+    const fetchUsers=()=>{
+      return axios.get('https://randomuser.me/api/')
+  }
+  const {data, isLoading,isError,error,isFetching,refetch} = useQuery(["results"],fetchUsers,{
+      enabled:true,
+      onSuccess:(res)=>{console.log('tahniah',res)
+  },
+  onError:(res)=>{console.log('error',res)}
+});
+// };
 
-useEffect(()=>{fetchUsers();},[]);
-console.log(users);
+// useEffect(()=>{fetchUsers();},[]);
+// console.log(users);
 
   return (
     <div style={{display:"flex",flexDirection:"column", justifyContent:'center',alignItems:"center"}}>
-        <img src={users.results[0].picture.medium} style={{borderRadius:"35%",width:"50%",height:"50%"}}/>
-      <h2>Hello {users.results[0].name.title} {users.results[0].name.first} {users.results[0].name.last} </h2>
-      <p>Email: {users.results[0].email} </p>
-      <p>Phone: {users.results[0].phone} </p>
-      <p>Cell: {users.results[0].cell} </p>
-      <p>Address: {users.results[0].location.street.number} {users.results[0].location.city} {users.results[0].location.state} {users.results[0].location.postcode} {users.results[0].location.country}</p>
+        <img src={data?.data?.results[0].picture.medium} style={{borderRadius:"35%",width:"50%",height:"50%"}}/>
+      <h2>Hello, {data?.data?.results[0].name.title} {data?.data?.results[0].name.first} {data?.data?.results[0].name.last} </h2>
+      <p>Email: {data?.data?.results[0].email} </p>
+      <p>Phone: {data?.data?.results[0].phone} </p>
+      <p>Cell: {data?.data?.results[0].cell} </p>
+      <p>Address: {data?.data?.results[0].location.street.number} {data?.data?.results[0].location.city} {data?.data?.results[0].location.state} {data?.data?.results[0].location.postcode} {data?.data?.results[0].location.country}</p>
     </div>
   );
 }
